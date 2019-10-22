245679857e4c989ef521f5279cc12b0259a53919
effprime <effprime@github.com>
2019-10-21 21:58:34 -0400

    Linting.

--- statuskeeper/code/statuskeeper.py
+++ statuskeeper/code/statuskeeper.py
@@ -1,143 +1,122 @@
-import requests
-import logging
+"""Provides detection status keeping using a persistent JSON file.
+"""
 import datetime
 import json
-import time
+import logging
 import os
+import time
+
+import requests
 
 LOOP_TIME = 30
 INITIAL_WAIT_TIME = 5
 JSON_FILE = "status.json"
 
-class StatusKeeper():
 
+class StatusKeeper:
+    """The object for performing detection status keeping.
+    """
 
     def __init__(self, cameras, api_url):
-        """
-        Initializes the StatusKeeper daemon.
+        """Initializes the StatusKeeper daemon.
 
-        cameras (int): The number of active cameras in Motion.
-        api_url (str): The URL for accessing the Motion API.
+        args:
+            cameras (int): The number of active cameras in Motion.
+            api_url (str): The URL for accessing the Motion API.
         """
-
         logging.getLogger().setLevel(logging.INFO)
-        logging.info("Starting status keeping with %s cameras" % cameras)
-        self.cameras = [str(i) for i in range(1,int(cameras)+1)]
-        self.APIURL = "http://%s" % (api_url)
+        logging.info("Starting status keeping with %s cameras", cameras)
+        self.status = {}
+        self.cameras = [str(i) for i in range(1, int(cameras) + 1)]
+        self.api_url = "http://%s" % (api_url)
         self.run()
 
+    @staticmethod
+    def get_api_command(info):
+        """Maps an API response chunk to the send chunk.
 
-    # private methods
-    def __get_API_command(self, info):
+        args:
+            info (str): API response chunk.
         """
-        Maps an API response chunk to the send chunk.
-
-        info (str): API response chunk.
-        """
-
         info = info.lower() if info else None
         if info == "active":
             return "start"
-        elif info == "pause":
-            return "pause"
-        return None
+        return info
 
-
-    def __initialize_status(self):
+    def initialize_status(self):
+        """Creates the status attribute with null entries.
         """
-        Creates the status attribute with null entries.
-        """
-
-        self.status = {}
         for cam in self.cameras:
             self.status[cam] = None
-        self.__set_status_timestamp()
+        self.set_status_timestamp()
 
-
-    def __set_status_timestamp(self):
+    def set_status_timestamp(self):
+        """Adds a timestamp entry to the status attribute.
         """
-        Adds a timestamp entry to the status attribute.
-        """
-
         self.status["time"] = str(datetime.datetime.now())
 
+    @staticmethod
+    def wait(time_):
+        """Wraps the sleep function with logging.
 
-    def __wait(self, time_):
+        args:
+            time (int): The amount of seconds to wait.
         """
-        Wraps the sleep function with logging.
-
-        time (int): The amount of seconds to wait.
-        """
-
-        logging.info("Sleeping for %s seconds" % (time_))
+        logging.info("Sleeping for %s seconds", time_)
         time.sleep(time_)
 
-
-    # public methods
-    def get_status_from_API(self):
+    def get_status_from_api(self):
+        """Updates status attribute from API responses.
         """
-        Updates status attribute from API responses.
-        """
-
         logging.info("Getting status from Motion API")
         for cam in self.cameras:
-            status = requests.get("%s/%s/detection/status" % (self.APIURL, cam)).text.split()[-1]
+            status = requests.get(
+                "%s/%s/detection/status" % (self.api_url, cam)
+            ).text.split()[-1]
             if status == "valid":
                 status = None
-            logging.info("API Camera %s status: %s" % (cam, status))
+            logging.info("API Camera %s status: %s", cam, status)
             self.status[cam] = status
-        self.__set_status_timestamp()
+        self.set_status_timestamp()
 
-
-    def get_status_from_JSON(self):
+    def get_status_from_json(self):
+        """Updates status attribute from local JSON file.
         """
-        Updates status attribute from local JSON file.
-        """
-
         logging.info("Getting current status from JSON")
         if os.path.exists(JSON_FILE):
-            with open('status.json', 'r') as file:
+            with open("status.json", "r") as file:
                 self.status = json.load(file)
             for cam, status in list(self.status.items())[:-1]:
-                logging.info("JSON Camera %s status: %s" % (cam, status))
+                logging.info("JSON Camera %s status: %s", cam, status)
         else:
             logging.info("JSON file not found -- new file will be created")
 
-
-    def log_status_to_JSON(self):
+    def log_status_to_json(self):
+        """Saves the status attribute to local JSON file.
         """
-        Saves the status attribute to local JSON file.
-        """
-
         logging.info("Saving current status to JSON")
-        with open(JSON_FILE, 'w') as file:
+        with open(JSON_FILE, "w") as file:
             json.dump(self.status, file)
 
-
-    def send_status_to_API(self):
+    def send_status_to_api(self):
+        """Sends commands to API based on status attribute.
         """
-        Sends commands to API based on status attribute.
-        """
-
         logging.info("Sending current status to API")
         for cam in self.cameras:
-            command = self.__get_API_command(self.status[cam])
+            command = StatusKeeper.get_api_command(self.status[cam])
             if command:
-                requests.get("%s/%s/detection/%s" % (self.APIURL, cam, command))
-            else: 
-                logging.error("Invalid status for Camera %s" % (cam))
-
+                requests.get("%s/%s/detection/%s" % (self.api_url, cam, command))
+            else:
+                logging.error("Invalid status for Camera %s", cam)
 
     def run(self):
+        """Runs startup functions and loops through get/save functions.
         """
-        Runs startup functions and loops through get/save functions.
-        """
-
-        self.__initialize_status()
-        self.get_status_from_JSON()
-        self.send_status_to_API()
-        self.__wait(INITIAL_WAIT_TIME)
+        self.initialize_status()
+        self.get_status_from_json()
+        self.send_status_to_api()
+        StatusKeeper.wait(INITIAL_WAIT_TIME)
         while True:
-            self.get_status_from_API()
-            self.log_status_to_JSON()
-            self.__wait(LOOP_TIME)
+            self.get_status_from_api()
+            self.log_status_to_json()
+            StatusKeeper.wait(LOOP_TIME)
