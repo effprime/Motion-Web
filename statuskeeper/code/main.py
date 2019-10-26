a521f5a0ab326f795109ce093a2e06a90c0e320e
effprime <effprime@github.com>
2019-10-26 17:53:53 -0400

    Add main check to statuskeeper startup.

--- statuskeeper/code/main.py
+++ statuskeeper/code/main.py
@@ -4,4 +4,5 @@ import sys
 
 from statuskeeper import StatusKeeper
 
-StatusKeeper(sys.argv[1], sys.argv[2], sys.argv[3])
+if __name__ == "__main__":
+    StatusKeeper(sys.argv[1], sys.argv[2], sys.argv[3])
