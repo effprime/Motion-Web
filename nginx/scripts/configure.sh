1d64aff2283b4b050f26a586f8317f2918ff037d
effprime <exdoubledot@gmail.com>
2019-12-07 19:59:37 -0800

    Remove configure scripts from Docker build

--- nginx/scripts/configure.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Add configuration for Nginx and Javascript
-sed -i "s/URL_VARIABLE/$1/g" /etc/nginx/conf.d/default.conf
-sed -i "s/API_PORT_VARIABLE/$2/g" /etc/nginx/conf.d/default.conf
-sed -i "s/URL_VARIABLE/$1/g" /web/camera_functions.js
-sed -i "s/API_PORT_VARIABLE/$2/g" /web/camera_functions.js
-sed -i "s/CAMERA_NUMBER_VARIABLE/$3/g" /web/camera_functions.js
